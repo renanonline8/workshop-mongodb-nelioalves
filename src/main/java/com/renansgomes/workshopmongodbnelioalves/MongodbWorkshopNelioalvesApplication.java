c81176cb64fac44543266b87154eb402fe44e694
Renan Santos Gomes <github@renansgomes.com.br>
2023-04-28 11:26:40 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/renansgomes/workshopmongodbnelioalves/MongodbWorkshopNelioalvesApplication.java
@@ -0,0 +1,13 @@
+package com.renansgomes.workshopmongodbnelioalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbWorkshopNelioalvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbWorkshopNelioalvesApplication.class, args);
+	}
+
+}
