36020c3ca7b556fac827ada9b8e11d86a1ac0b44
Renan Santos Gomes <github@renansgomes.com.br>
2023-05-03 12:39:08 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/renansgomes/workshopmongodbnelioalves/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/renansgomes/workshopmongodbnelioalves/resources/exception/ResourceExceptionHandler.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(ObjectNotFoundException.class);
+	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
